a024b708da477a85fc45ce508505a99e1f3b9b4d
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2016-11-30 15:46:08 +0000

    Initial application setup

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-
 # paye-registration-frontend
 
-[![Build Status](https://travis-ci.org/hmrc/paye-registration-frontend.svg?branch=master)](https://travis-ci.org/hmrc/paye-registration-frontend) [ ![Download](https://api.bintray.com/packages/hmrc/releases/paye-registration-frontend/images/download.svg) ](https://bintray.com/hmrc/releases/paye-registration-frontend/_latestVersion)
+[![Build Status](https://travis-ci.org/hmrc/paye-registration-frontend.svg)](https://travis-ci.org/hmrc/paye-registration-frontend) [ ![Download](https://api.bintray.com/packages/hmrc/releases/paye-registration-frontend/images/download.svg) ](https://bintray.com/hmrc/releases/paye-registration-frontend/_latestVersion)
 
 This is a placeholder README.md for a new repository
 
 ### License
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
-    
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html")
---
+++ project/FrontendBuild.scala
@@ -0,0 +1,59 @@
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object FrontendBuild extends Build with MicroService {
+
+  val appName = "paye-registration-frontend"
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.sbt.PlayImport._
+  import play.core.PlayVersion
+
+  private val playHealthVersion = "2.0.0"
+  private val logbackJsonLoggerVersion = "3.1.0"
+  private val frontendBootstrapVersion = "7.10.0"
+  private val govukTemplateVersion = "5.0.0"
+  private val playUiVersion = "5.3.0"
+  private val playPartialsVersion = "5.2.0"
+  private val playAuthorisedFrontendVersion = "6.2.0"
+  private val playConfigVersion = "3.0.0"
+  private val hmrcTestVersion = "2.2.0"
+  private val scalaTestVersion = "2.2.6"
+  private val pegdownVersion = "1.6.0"
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
+    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
+    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-ui" % playUiVersion
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+        "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
+        "org.pegdown" % "pegdown" % pegdownVersion % scope,
+        "org.jsoup" % "jsoup" % "1.8.1" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
