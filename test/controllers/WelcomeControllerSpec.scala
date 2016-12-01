ce953a6c1663c95d1ef92501af6829fb6e1df28a
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2016-12-01 11:01:42 +0000

    Add Welcome controller initial tests

---
+++ test/controllers/WelcomeControllerSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import helpers.PAYERegSpec
+import play.api.http.Status
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class WelcomeControllerSpec extends PAYERegSpec {
+
+  val fakeRequest = FakeRequest("GET", "/start")
+
+
+  "GET /start" should {
+    "return 200" in {
+      val result = WelcocmeController.show(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+
+    "return HTML" in {
+      val result = WelcocmeController.show(fakeRequest)
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+
+
+  }
+
+}
