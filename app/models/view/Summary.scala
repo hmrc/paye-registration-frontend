8ed07c3b792fceef99f14463bcb68a04f57625b4
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-01-17 17:23:27 +0000

    Add Summary view model

---
+++ app/models/view/Summary.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.view
+
+import play.api.mvc.Call
+
+case class Summary(
+                  sections: Seq[SummarySection]
+                  ) {
+
+}
+
+case class SummarySection(
+                        id: String,
+                        rows: Seq[SummaryRow]
+                        )
+
+case class SummaryRow(
+                      id: String,
+                      answer: Option[String],
+                      changeLink: Option[Call]
+                      )
