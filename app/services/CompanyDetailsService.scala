9abcf16e63f5c0e4c4147b4da130dc371fbe83d6
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-02-07 18:04:01 +0000

    Update getCompanyDetails. Tests broken

--- app/services/CompanyDetailsService.scala
+++ app/services/CompanyDetailsService.scala
@@ -22,8 +22,6 @@ import connectors._
 import enums.{CacheKeys, DownstreamOutcome}
 import models.view.{Address, CompanyDetails => CompanyDetailsView, TradingName => TradingNameView}
 import models.api.{CompanyDetails => CompanyDetailsAPI}
-import common.exceptions.InternalExceptions.APIConversionException
-import play.api.Logger
 import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.Future
@@ -31,16 +29,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class CompanyDetailsService @Inject()(
-                                       keystoreConn: KeystoreConnector,
-                                       payeRegConn: PAYERegistrationConnector,
-                                       coHoSrv: CoHoAPIService,
-                                       s4LSrv: S4LService
+                                       injKeystoreConnector: KeystoreConnector,
+                                       injPAYERegistrationConnector: PAYERegistrationConnector,
+                                       injCoHoAPIService: CoHoAPIService,
+                                       injS4LService: S4LService
                                      )
   extends CompanyDetailsSrv {
-  override val keystoreConnector = keystoreConn
-  override val payeRegConnector = payeRegConn
-  override val cohoService = coHoSrv
-  override val s4LService = s4LSrv
+  override val keystoreConnector = injKeystoreConnector
+  override val payeRegConnector = injPAYERegistrationConnector
+  override val cohoService = injCoHoAPIService
+  override val s4LService = injS4LService
 }
 
 trait CompanyDetailsSrv extends CommonService {
@@ -52,64 +50,57 @@ trait CompanyDetailsSrv extends CommonService {
   def getCompanyDetails()(implicit hc: HeaderCarrier): Future[CompanyDetailsView] = {
     s4LService.fetchAndGet[CompanyDetailsView](CacheKeys.CompanyDetails.toString) flatMap {
       case Some(companyDetails) => Future.successful(companyDetails)
-      case None => {
-        for {
-          regID <- fetchRegistrationID
-          regResponse <- payeRegConnector.getCompanyDetails(regID)
-          details <- regResponse
-        } yield apiToView(details)
-      }.recoverWith {
-        case _: NoSuchElementException => for {
-          companyName <- cohoService.getStoredCompanyName()
-        } yield CompanyDetailsView(None, companyName, None, None)
-      }
+      case None => for {
+        regID    <- fetchRegistrationID
+        oDetails <- payeRegConnector.getCompanyDetails(regID)
+        details  <- convertOrCreateCompanyDetailsView(oDetails)
+      } yield details
     }
   }
 
-  def getCompanyName(detailsViewOption: Option[CompanyDetailsView])(implicit hc: HeaderCarrier): Future[String] = {
-    detailsViewOption.map {
-      detailsView => Future.successful(detailsView.companyName)
-    }.getOrElse {
-      cohoService.getStoredCompanyName()
+  private[services] def convertOrCreateCompanyDetailsView(oAPI: Option[CompanyDetailsAPI])(implicit hc: HeaderCarrier): Future[CompanyDetailsView] = {
+    oAPI match {
+      case Some(detailsAPI) => Future.successful(apiToView(detailsAPI))
+      case None => for {
+        cName   <- cohoService.getStoredCompanyName
+        details <- s4LService.saveForm[CompanyDetailsView](CacheKeys.CompanyDetails.toString, CompanyDetailsView(None, cName, None, None))
+       } yield CompanyDetailsView(None, cName, None, None)
     }
+  }
+
+  private def saveCompanyDetails(viewModel: CompanyDetailsView)(implicit hc: HeaderCarrier): Future[DownstreamOutcome.Value] = {
+    viewToAPI(viewModel) fold(
+      incompleteView =>
+        s4LService.saveForm[CompanyDetailsView](CacheKeys.CompanyDetails.toString, incompleteView) map {_ => DownstreamOutcome.Success},
+      completeAPI =>
+        for {
+          regID     <- fetchRegistrationID
+          details   <- payeRegConnector.upsertCompanyDetails(regID, completeAPI)
+          clearData <- s4LService.clear
+        } yield DownstreamOutcome.Success
+      )
   }
 
   def submitTradingName(tradingNameView: TradingNameView)(implicit hc: HeaderCarrier): Future[DownstreamOutcome.Value] = {
     for {
-      regID <- fetchRegistrationID
-      oCompanyDetails <- getCompanyDetails
-      updatedDetails <- addTradingNameToCompanyDetails(tradingNameView, oCompanyDetails)
-      outcome <- submitCompanyDetails(updatedDetails, regID)
+      details <- getCompanyDetails
+      outcome <- saveCompanyDetails(details.copy(tradingName = Some(tradingNameView)))
     } yield outcome
   }
 
-  def getROAddress()(implicit hc: HeaderCarrier): Future[Address] = ???
+  def getROAddress()(implicit hc: HeaderCarrier): Future[Address] = {
+    for {
+      regID   <- fetchRegistrationID
+      details <- getCompanyDetails
+      //TODO: Add connector call for undefined address when connector implemented
+    } yield details.address.get
+  }
 
   def submitROAddress(address: Address)(implicit hc: HeaderCarrier): Future[DownstreamOutcome.Value] = {
     for {
-      regID <- fetchRegistrationID
-      oCompanyDetails <- getCompanyDetails
-      updatedDetails <- oCompanyDetails.copy(address = Some(address))
-      outcome <- submitCompanyDetails(updatedDetails, regID)
+      details <- getCompanyDetails
+      outcome <- saveCompanyDetails(details.copy(address = Some(address)))
     } yield outcome
-  }
-
-  private[services] def addTradingNameToCompanyDetails(tradingNameView: TradingNameView, oCompanyDetails: CompanyDetailsView)
-                                                      (implicit hc: HeaderCarrier): Future[CompanyDetailsView] = {
-    oCompanyDetails match {
-      case Some(companyDetails) => Future.successful()
-      case None => cohoService.getStoredCompanyName() map {
-        cName => CompanyDetailsView(None, cName, Some(tradingNameView), None)
-      }
-    }
-  }
-
-  private[services] def submitCompanyDetails(detailsView: CompanyDetailsView, regID:String)(implicit hc: HeaderCarrier): Future[DownstreamOutcome.Value] = {
-    payeRegConnector.upsertCompanyDetails(regID, viewToAPI(detailsView)) map {
-      _ => DownstreamOutcome.Success
-    } recover {
-      case _ => DownstreamOutcome.Failure
-    }
   }
 
   private[services] def apiToView(apiModel: CompanyDetailsAPI): CompanyDetailsView = {
@@ -127,13 +118,14 @@ trait CompanyDetailsSrv extends CommonService {
     )
   }
 
-  private def tradingNameAPIValue(tradingNameView: TradingNameView): Option[String] =
-    if( tradingNameView.differentName ) tradingNameView.tradingName else None
-
   private[services] def viewToAPI(viewData: CompanyDetailsView): Either[CompanyDetailsView, CompanyDetailsAPI] = viewData match {
     case CompanyDetailsView(crn, companyName, Some(tradingName), Some(address)) =>
-      Right(CompanyDetailsAPI(crn, companyName, tradingNameAPIValue(tradingName), address)
+      Right(CompanyDetailsAPI(crn, companyName, tradingNameAPIValue(tradingName), address))
     case _ => Left(viewData)
+  }
+
+  private def tradingNameAPIValue(tradingNameView: TradingNameView): Option[String] = {
+    if (tradingNameView.differentName) tradingNameView.tradingName else None
   }
 
 }
