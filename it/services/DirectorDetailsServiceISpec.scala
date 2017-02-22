3b83ab7e578b9640dd9554b8b6183542da8a7821
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-02-22 09:11:06 +0000

    WIP

---
+++ it/services/DirectorDetailsServiceISpec.scala
@@ -0,0 +1,37 @@
+package services
+
+import itutil.{WiremockHelper, IntegrationSpecBase}
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.play.http.HeaderCarrier
+
+class DirectorDetailsServiceISpec extends IntegrationSpecBase {
+
+  val mockHost = WiremockHelper.wiremockHost
+  val mockPort = WiremockHelper.wiremockPort
+  val mockUrl = s"http://$mockHost:$mockPort"
+
+  val additionalConfiguration = Map(
+    "microservice.services.paye-registration.host" -> s"$mockHost",
+    "microservice.services.paye-registration.port" -> s"$mockPort",
+    "microservice.services.cachable.session-cache.host" -> s"$mockHost",
+    "microservice.services.cachable.session-cache.port" -> s"$mockPort",
+    "microservice.services.cachable.short-lived-cache.host" -> s"$mockHost",
+    "microservice.services.cachable.short-lived-cache.port" -> s"$mockPort",
+    "microservice.services.coho-api.host" -> s"$mockHost",
+    "microservice.services.coho-api.port" -> s"$mockPort",
+    "application.router" -> "testOnlyDoNotUseInAppConf.Routes"
+  )
+
+  override implicit lazy val app: Application = new GuiceApplicationBuilder()
+    .configure(additionalConfiguration)
+    .build
+
+  val regId = "12345"
+  implicit val hc = HeaderCarrier()
+
+  "getCompanyDetails" should {
+
+  }
+
+}
