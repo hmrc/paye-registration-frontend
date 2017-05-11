1b9d7a2d8af62ac2c09bf9a7df900843677f88df
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-05-11 15:59:18 +0100

    Update Address reads to default to country in case of invalid postcode

--- app/models/Address.scala
+++ app/models/Address.scala
@@ -18,6 +18,7 @@ package models
 
 import play.api.data.validation.ValidationError
 import play.api.libs.json._
+import utils.Validators
 
 case class Address(
                     line1: String,
@@ -33,31 +34,28 @@ object Address {
 
   val adressLookupReads: Reads[Address] = new Reads[Address] {
     def reads(json: JsValue): JsResult[Address] = {
-      val address = json.\("address").as[JsObject]
-      val postcode = address.\("postcode").asOpt[String]
-      val lines = address.\("lines").as[JsArray].as[List[String]]
 
-      val oCountry = address.\("country").asOpt[JsObject]
-      val countryCode = oCountry.map(_.\("code").get.as[String])
-
-      if(postcode.isEmpty && countryCode.isEmpty) {
-        JsError(ValidationError("neither string nor country were defined"))
-      } else if(lines.length < 2) {
-        JsError(ValidationError(s"only ${lines.length} lines provided from address-lookup-frontend"))
-      } else {
-        val L3 = if(lines.isDefinedAt(2)) Some(lines(2)) else None
-        val L4 = if(lines.isDefinedAt(3)) Some(lines(3)) else None
-
-        val addr = Address(
-          lines.head,
-          lines(1),
-          L3,
-          L4,
-          postcode,
-          {if(postcode.isDefined) None else countryCode}
-        )
-        JsSuccess(addr)
+      val validatedPostcode = json.\("address").\("postcode").asOpt[String] match {
+        case Some(pc) if pc.matches(Validators.postcodeRegex) => Right(pc)
+        case Some(pc) => Left("Invalid postcode")
+        case _ => Left("No postcode")
       }
+
+      val addressLines = json.\("address").\("lines").as[JsArray].as[List[String]]
+      val countryName = json.\("address").\("country").\("name").asOpt[String]
+
+      (validatedPostcode, countryName, addressLines) match {
+        case (Left(msg), None, _)              => JsError(ValidationError(s"$msg and no country to default to"))
+        case (_, _, lines) if lines.length < 2 => JsError(ValidationError(s"only ${lines.length} lines provided from address-lookup-frontend"))
+        case (Left(msg), c @ Some(_), lines)   => JsSuccess(makeAddress(None, c, lines))
+        case (Right(pc), _, lines)             => JsSuccess(makeAddress(Some(pc), None, lines))
+      }
+    }
+
+    def makeAddress(postCode: Option[String], country: Option[String], lines: List[String]) = {
+      val L3 = if(lines.isDefinedAt(2)) Some(lines(2)) else None
+      val L4 = if(lines.isDefinedAt(3)) Some(lines(3)) else None
+      Address(lines.head,lines(1),L3,L4,postCode,country)
     }
   }
 }
