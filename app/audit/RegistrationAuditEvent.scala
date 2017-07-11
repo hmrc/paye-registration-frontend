a01f614d3e517fe1f4794796956c6c1a06b94b4e
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2017-07-11 09:32:02 +0100

    Push base auditing tools for future use

---
+++ app/audit/RegistrationAuditEvent.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import audit.RegistrationAuditEvent.buildTags
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.{AnyContent, Request}
+import uk.gov.hmrc.play.audit.model.ExtendedDataEvent
+import uk.gov.hmrc.play.http.HeaderCarrier
+
+case class TagSet(clientIP: Boolean,
+                  clientPort: Boolean,
+                  requestId: Boolean,
+                  sessionId: Boolean,
+                  deviceId: Boolean,
+                  authorisation: Boolean,
+                  path: Boolean)
+
+object TagSet {
+  val ALL_TAGS = TagSet(true, true, true, true, true, true, true)
+  val NO_TAGS = TagSet(false, false, false, false, false, false, false)
+  val REQUEST_ONLY = TagSet(false, false, true, false, false, false, false)
+}
+
+import audit.TagSet.ALL_TAGS
+
+abstract class RegistrationAuditEvent(auditType: String, transactionName : Option[String], detail: JsObject, tagSet: TagSet = ALL_TAGS)
+                                     (implicit hc: HeaderCarrier, optReq: Option[Request[AnyContent]] = None)
+  extends ExtendedDataEvent(
+    auditSource = "paye-registration",
+    auditType = auditType,
+    detail = detail,
+    tags = buildTags(transactionName.getOrElse(auditType), tagSet)
+  )
+
+object RegistrationAuditEvent {
+
+  val EXTERNAL_ID = "externalId"
+  val EXTERNAL_USER_ID = "externalUserId"
+  val AUTH_PROVIDER_ID = "authProviderId"
+  val JOURNEY_ID = "journeyId"
+  val PATH = "path"
+
+  def buildTags(transactionName: String, tagSet: TagSet)(implicit hc: HeaderCarrier, optReq: Option[Request[AnyContent]]) = {
+    Map("transactionName" -> transactionName) ++
+      buildClientIP(tagSet) ++
+      buildClientPort(tagSet) ++
+      buildRequestId(tagSet) ++
+      buildSessionId(tagSet) ++
+      buildDeviceId(tagSet) ++
+      buildAuthorization(tagSet) ++
+      buildPath(tagSet)
+  }
+
+  private def buildClientIP(tagSet: TagSet)(implicit hc: HeaderCarrier) =
+    if(tagSet.clientIP) Map("clientIP" -> hc.trueClientIp.getOrElse("-")) else Map()
+
+  private def buildClientPort(tagSet: TagSet)(implicit hc: HeaderCarrier) =
+    if(tagSet.clientPort) Map("clientPort" -> hc.trueClientPort.getOrElse("-")) else Map()
+
+  private def buildRequestId(tagSet: TagSet)(implicit hc: HeaderCarrier) =
+    if(tagSet.requestId) Map(hc.names.xRequestId -> hc.requestId.map(_.value).getOrElse("-")) else Map()
+
+  private def buildSessionId(tagSet: TagSet)(implicit hc: HeaderCarrier) =
+    if(tagSet.sessionId) Map(hc.names.xSessionId -> hc.sessionId.map(_.value).getOrElse("-")) else Map()
+
+  private def buildAuthorization(tagSet: TagSet)(implicit hc: HeaderCarrier) =
+    if(tagSet.authorisation) Map(hc.names.authorisation -> hc.authorization.map(_.value).getOrElse("-")) else Map()
+
+  private def buildDeviceId(tagSet: TagSet)(implicit hc: HeaderCarrier) =
+    if(tagSet.deviceId) Map(hc.names.deviceID -> hc.deviceID.getOrElse("-")) else Map()
+
+  private def buildPath(tagSet: TagSet)(implicit optReq: Option[Request[AnyContent]]) = {
+    if (tagSet.path) optReq.fold(Map[String, String]())(req => Map(PATH -> req.path)) else Map()
+  }
+}
+
+class AmendCompletionCapacityEvent(regId: String, details: JsObject)(implicit hc: HeaderCarrier)
+  extends RegistrationAuditEvent("completionCapacityAmendment", None, details.++(Json.obj(RegistrationAuditEvent.JOURNEY_ID -> regId)))(hc)
---
+++ test/audit/RegistrationAuditEventSpec.scala
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.play.audit.model.ExtendedDataEvent
+import uk.gov.hmrc.play.http.HeaderCarrier
+import uk.gov.hmrc.play.http.logging.{Authorization, ForwardedFor, RequestId, SessionId}
+import uk.gov.hmrc.play.test.UnitSpec
+
+class RegistrationAuditEventSpec extends UnitSpec {
+
+  "RegistrationEvent" should {
+    val clientIP: String = "1.2.3.4"
+    val clientPort: String = "1234"
+    val auditType = "testType"
+    val bearer = "Bearer 12345"
+    val session: String = "sess"
+    val request: String = "req"
+    val device = "device"
+
+    val completeCarrier = HeaderCarrier(
+      trueClientIp = Some(clientIP),
+      trueClientPort = Some(clientPort),
+      forwarded = Some(ForwardedFor("2.3.4.5")),
+      sessionId = Some(SessionId(session)),
+      requestId = Some(RequestId(request)),
+      authorization = Some(Authorization(bearer)),
+      deviceID = Some(device)
+    )
+
+    val emptyCarrier = HeaderCarrier()
+
+    "have the correct tags for a full header carrier" in {
+      val event = new RegistrationAuditEvent(auditType, None, Json.obj())(completeCarrier) {}
+
+      val result = Json.toJson[ExtendedDataEvent](event)
+
+      val expectedTags: JsObject = Json.obj(
+        "clientIP"        -> clientIP,
+        "clientPort"      -> clientPort,
+        "X-Request-ID"    -> request,
+        "X-Session-ID"    -> session,
+        "transactionName" -> auditType,
+        "deviceID"        -> device,
+        "Authorization"   -> bearer
+      )
+
+      (result \ "tags").as[JsObject] shouldBe expectedTags
+    }
+
+    "have the correct tags for an empty header carrier" in {
+      val event = new RegistrationAuditEvent(auditType, None, Json.obj())(emptyCarrier) {}
+
+      val result = Json.toJson[ExtendedDataEvent](event)
+
+      val expectedTags: JsObject = Json.obj(
+        "clientIP"        -> "-",
+        "clientPort"      -> "-",
+        "X-Request-ID"    -> "-",
+        "X-Session-ID"    -> "-",
+        "transactionName" -> auditType,
+        "Authorization"   -> "-",
+        "deviceID"        -> "-"
+      )
+
+      (result \ "tags").as[JsObject] shouldBe expectedTags
+    }
+
+    "Output with minimum tags" in {
+      val event = new RegistrationAuditEvent(auditType, None, Json.obj(), TagSet.NO_TAGS)(completeCarrier) {}
+
+      val result = Json.toJson[ExtendedDataEvent](event)
+
+      val expectedTags: JsObject = Json.obj(
+        "transactionName" -> auditType
+      )
+
+      (result \ "tags").as[JsObject] shouldBe expectedTags
+    }
+
+    "Output with name and clientIP/Port tags" in {
+      val tagSet = TagSet.NO_TAGS.copy(clientIP = true, clientPort = true)
+      val event = new RegistrationAuditEvent(auditType, None, Json.obj(), tagSet)(completeCarrier) {}
+
+      val result = Json.toJson[ExtendedDataEvent](event)
+
+      val expectedTags: JsObject = Json.obj(
+        "transactionName" -> auditType,
+        "clientIP" -> clientIP,
+        "clientPort" -> clientPort
+      )
+
+      (result \ "tags").as[JsObject] shouldBe expectedTags
+    }
+
+    "output with name, request, session & authz tags" in {
+      val tagSet = TagSet.ALL_TAGS.copy(clientIP = false, clientPort = false, deviceId = false)
+      val event = new RegistrationAuditEvent(auditType, None, Json.obj(), tagSet)(completeCarrier) {}
+
+      val result = Json.toJson[ExtendedDataEvent](event)
+
+      val expectedTags: JsObject = Json.obj(
+        "X-Request-ID" -> request,
+        "X-Session-ID" -> session,
+        "transactionName" -> auditType,
+        "Authorization" -> bearer
+      )
+
+      (result \ "tags").as[JsObject] shouldBe expectedTags
+    }
+
+
+    "have the correct result" in {
+      val event = new RegistrationAuditEvent(auditType, None, Json.obj())(completeCarrier) {}
+
+      val result = Json.toJson[ExtendedDataEvent](event)
+
+      (result \ "auditSource").as[String] shouldBe "paye-registration"
+      (result \ "auditType").as[String] shouldBe auditType
+
+      (result \ "detail").as[JsObject] shouldBe Json.obj()
+    }
+  }
+}
