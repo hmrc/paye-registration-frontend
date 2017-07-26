22a1c4e167a9522934f5540e1c08df52c377e7c7
kalyan.kallamadi <kalyan.kallamadi@digital.hmrc.gov.uk>
2017-07-26 13:56:58 +0100

    removing full stop and space from the title which was used for testing

--- app/connectors/test/TestIncorpInfoConnector.scala
+++ app/connectors/test/TestIncorpInfoConnector.scala
@@ -79,7 +79,7 @@ trait TestIncorpInfoConnect {
          |        },
          |        {
          |            "name_elements" : {
-         |                "title" : "M x.",
+         |                "title" : "Mx",
          |                "forename" : "Jingly",
          |                "surname" : "Jingles"
          |            },
