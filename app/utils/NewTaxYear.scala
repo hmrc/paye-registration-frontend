5f609ce14aaa6b3c0f3cdef6d90c99d6248eb8b2
Alex Rimmer <31283208+AlexRimmerHMRC@users.noreply.github.com>
2019-08-08 12:42:10 +0100

    Updated content of confirmation page

--- app/utils/NewTaxYear.scala
+++ app/utils/NewTaxYear.scala
@@ -20,7 +20,7 @@ import java.time.LocalDate
 
 object NewTaxYear {
   val startPeriod = LocalDate.of(SystemDate.getSystemDate.getYear, 2, 6)
-  val endPeriod   = LocalDate.of(SystemDate.getSystemDate.getYear, 12, 1)
+  val endPeriod   = LocalDate.of(SystemDate.getSystemDate.getYear, 5, 17)
 
   val taxYearStart = LocalDate.of(SystemDate.getSystemDate.getYear, 4, 6)
 }
